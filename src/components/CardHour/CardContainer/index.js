09d11f0d70782b13032c08a333e1b487e7dbf907
Luan Rodrigues <luanrodrigues007@hotmail.com>
2023-02-21 02:10:37 -0300

    fix: missing block div in card hourds

--- src/components/CardHour/CardContainer/index.js
+++ src/components/CardHour/CardContainer/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export default function CardContainer({ label, children }) {
-    return <section
-        className='rounded-md border-purple bg-darkModeElevated border-2 inline-block pt-16 pb-10 px-10 min-w-[90vw] sm:min-w-[45vw] md:min-w-[40vw] lg:min-w-[35vw] text-left mb-10 relative'>
+    return <div>
+        <section
+            className='rounded-md border-purple bg-darkModeElevated border-2 inline-block pt-16 pb-10 px-10 min-w-[90vw] sm:min-w-[45vw] md:min-w-[40vw] lg:min-w-[35vw] text-left mb-10 relative'>
                 <span className='absolute -top-7 bg-purple rounded-full p-3 left-10 right-10 text-center'>
                     {label}
                 </span>
 
-        {children}
-    </section>;
+            {children}
+        </section>
+    </div>;
 }
