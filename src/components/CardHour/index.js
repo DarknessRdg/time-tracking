e4383b2dd99f92b100a0a722f7ff8c0c2bbbf43c
Luan Rodrigues <luanrodrigues007@hotmail.com>
2023-02-20 23:53:44 -0300

    refactor: create card container component

--- src/components/CardHour/index.js
+++ src/components/CardHour/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Trash from '../../icons/Trash';
+import CardContainer from './CardContainer';
 
 const ON_GOING_LABEL = 'Em andamento...';
 
@@ -12,42 +13,26 @@ export default function CardHour({ hour, onClose, onDelete }) {
         period = 'Duração: ' + hour.period().toString();
     }
 
-    return (
-        <div>
-            <section
-                className={
-                    'rounded-md border-purple bg-darkModeElevated border-2 inline-block pt-16 pb-10 px-10 min-w-[90vw] sm:min-w-[45vw] md:min-w-[40vw] lg:min-w-[35vw] text-left mb-10 relative'
-                }
-            >
-                <span
-                    className={
-                        'absolute -top-7 bg-purple rounded-full p-3 left-10 right-10 text-center'
-                    }
-                >
-                    {period}
-                </span>
-
-                <div className="mb-5">
-                    <p>Início: {hour.start().toString()}</p>
-                    <p>Fim: {end}</p>
-                </div>
-
-                <button
-                    className={'py-2 px-4 bg-red-600 rounded-full text-blue-50'}
-                    onClick={onClose}
-                >
-                    Encerrar
-                </button>
-
-                <button
-                    type={'button'}
-                    title={'Deletar ponto'}
-                    onClick={onDelete}
-                    className={'float-right text-red-500 text-2xl'}
-                >
-                    <Trash />
-                </button>
-            </section>
+    return <CardContainer label={period}>
+        <div className='mb-5'>
+            <p>Início: {hour.start().toString()}</p>
+            <p>Fim: {end}</p>
         </div>
-    );
+
+        <button
+            className={'py-2 px-4 bg-red-600 rounded-full text-blue-50'}
+            onClick={onClose}
+        >
+            Encerrar
+        </button>
+
+        <button
+            type={'button'}
+            title={'Deletar ponto'}
+            onClick={onDelete}
+            className={'float-right text-red-500 text-2xl'}
+        >
+            <Trash />
+        </button>
+    </CardContainer>;
 }
